ffe4d156675a44a7dcf75f8a3f341a8b1466a3fa
Jenny Lee <jenny128l@naver.com>
2023-03-07 10:22:29 +0900

    aa

--- 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/js-autoScroll.js
+++ 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/js-autoScroll.js
@@ -1,48 +1,49 @@
 // JS로 구현한 자동페이지 휠 JS : js-autoScroll.js
 
-// 새로고침할대 스크롤위치 캐싱 무시하고 맨위로 이동!
-// scroollTo(가로,세로) -> 위치이동 메서드
+// 새로고침할때 스크롤위치 캐싱 무시하고 맨위로 이동!
+// scrollTo(가로,세로) -> 위치이동 메서드
 setTimeout(() => {
-    window.scrollTo(0,0);
-},100);
+    window.scrollTo(0, 0);
+}, 100);
 
-// 로딩함수 호출 /////
-window.addEventListener("DOMContentLoaded",loadFn);
+// 로딩함수 호출 /////////
+window.addEventListener("DOMContentLoaded", loadFn);
 
-function loadFn(){
-
+function loadFn() {
     // 호출확인
     console.log("로딩완료!");
 
-    /****************************************************
+    /************************************************* 
         [ 휠 이벤트를 이용한 페이지 이동 컨트롤하기! ]
         -> 휠 이벤트명 : wheel
-        (예전엔 mousewheel 이벤트가 사용됨 - 공식적으로 폐기됨 )
-        1. 사용법 :
-            (1) 이벤트 속성에 함수를 할당
+        (예전엔 mousewheel 이벤트가 사용됨 - 공식적으로 폐기됨)
+        
+        1. 사용법 : 
+            (1) 이벤트속성에 함수를 할당
                 요소.onwheel = 함수
             (2) 브라우저 이벤트에 등록함
-                요소.addEventListener("wheel",함수,{옵션})
+                요소.addEventListenr("wheel",함수,{옵션})
+
         2. 포인트 :
-            (1) 기존 췰이동 기능은 정지한다!
+            (1) 기존 휠이동 기능은 정지한다!
             -> event.preventDefault()
             (2) 휠 방향을 알아내어 기능과 매칭한다!
-            ->  event.wheelDelta (휠방향속성)
+            -> event.wheelDelta (휠방향속성)
             (3) 페이지번호를 전역적으로 사용한다!
-            -> let pgnum = 0 
-        
-        3. 휠 이벤트 대상:
+            -> let pgnum = 0
+
+        3. 휠 이벤트 대상: 
             (1) 전체 스크롤바인 경우 : window
             (2) 개별박스인 경우 : 선택요소
 
         4. scroll 이벤트와 wheel 이벤트 비교
-            (1) 곧통점 : 스크롤되는 페이지의 위치이동
+            (1) 공통점 : 스크롤되는 페이지의 위치이동
             (2) 차이점 : 
-                마우스 휠의 동작에만 반응하는 이벤트는 wheel 
-                스크롤바의 이동에 반응하는 이벤트는 scroll 
-                
-        ____________________________________________________________
-        
+                마우스 휠의 동작에만 반응하는 이벤트는 wheel
+                스크롤바의 이동에 반응하는 이벤트는 scroll
+
+        ______________________________________________
+
         [ addEventListener 메서드의 옵션에 관하여 ]
 
         기존 addEventListener의 3번째 파라미터로 캡쳐링/버블링 여부를 
@@ -68,62 +69,67 @@ function loadFn(){
                 preventDefault를 사용할 수 없음. 크롬 51부터 지원
                 이 중, passive 속성은 성능향상을 위해, 
                 브라우저의 기능을 프로그래밍으로 제어할수 있음
-                _______________________________________________________
+                ___________________________________________
 
-                -> 최근 업데이트 된 브라우저는 passive 기본값이 
+                -> 최근 업데이트 된 브라우저는 passive기본값이
                 true로 셋팅됨으로 window, documement, body 이 세가지
                 객체대해 스크롤 막기기능을 비활성화 하였다!!!
                 따라서 기본기능을 막고자 하면 passive:false
                 로 기능을 활성화 해야한다!!!
 
+    *************************************************/
 
-    ****************************************************/
-   
     // 0. 변수 설정하기
-    // 전체 페이지변수
-    let pgnum = 0; //현재 페이지번호(첫페이지 0)
+    // (1) 전체 페이지변수
+    let pgnum = 0; // 현재 페이지번호(첫페이지 0)
+    // (2) 전체 페이지수
+    const pgcnt = document.querySelectorAll(".page").length;
+    console.log("전체페이지수:", pgcnt);
+    // (3) 광스크롤 금지변수(0-허용,1-불허용)
+    let prot_sc = 0;
 
+    // 1. 전체 휠 이벤트 설정하기 ////////
+    window.addEventListener("wheel", wheelFn, { passive: false });
 
-    //  1. 전체 휠 이벤트 설정하기 /////////
-    window.addEventListener("wheel",wheelFn,{passive:false});
-
-    // 2. 휠 이벤트 함수 만들기 /////////
-    function wheelFn(e){ // e - 이벤트 전달변수
+    // 2. 휠 이벤트 함수 만들기 ///////////
+    function wheelFn(e) {
+        // e - 이벤트 전달변수
         // (0) 기본기능 멈추기
         // addEventListener 옵션 passive:false 필수!
         e.preventDefault();
 
         // (1) 호출확인
         // console.log("휠~~~~");
-        
-        // (2) 췰 방향 알아내기
+
+        // (2) 휠 방향 알아내기
         // 이벤트객체.wheelDelta
         let dir = e.wheelDelta;
-        
-        console.log("방향:",dir);
+
+        console.log("방향:", dir);
         // 휠내리면 마이너스(올리면 플러스)
 
         // (3) 방향에 따른 페이지번호 증감
-        // 스크롤 아랫방향 : 페이지번호 증가!
-        if(dir<0) {
+        // 스크롤 아랫방향 : 페이지번호 증가
+        if (dir < 0) {
+            // 페이지번호 1씩증가
             pgnum++;
-            if(pgnum>6)pgnum = 6;
-        }
-        // 스크롤 윗방향 : 페이지번호 감소!
-        else{
-            pgnum--;
-            if(pgnum<0) pgnum = 0;
-        } 
+            // 한계수 : 페이지 끝번호(페이지수-1)
+            if (pgnum > pgcnt - 1) pgnum = pgcnt - 1;
+        } /////// if ////////
 
-        console.log("페이지번호:",pgnum);
+        // 스크롤 윗방향 : 페이지번호 감소
+        else {
+            // 페이지번호 1씩감소
+            pgnum--;
+            // 한계수 : 페이지 첫번호 -> 0
+            if (pgnum < 0) pgnum = 0;
+        } //////// else /////////
+
+        console.log("페이지번호:", pgnum);
 
         // (4) 페이지 이동하기
         // scrollTo(가로,세로)
-        window.scrollTo(0,window.innerHeight*pgnum);
+        window.scrollTo(0, window.innerHeight * pgnum);
         // 세로 이동위치: 윈도우높이값*페이지번호
-
-
-
-    } ////// wheelFn 함수 /////////
-
-} //////// loadFn 함수 ///////
+    } ////////////// wheelFn 함수 //////////
+} ////////////// loadFn 함수 ///////////////////
